2b897f947e5ef5d5e1c10eb5ddc13e9d34346ac3
fajarutamaa <fajardwiutomo75@gmail.com>
2023-11-30 19:55:55 +0700

    add forgot and reset password

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,7 @@ const authRoute = require('../routes/auth/auth.route')
 const userRoute = require('../routes/user.route')
 const activationRoute = require('./activation.route')
 const todoRoute = require('../routes/todo.route')
+const { ForgotPassword, ResetPassword } = require('../controllers/auth/auth.controller')
 
 router.use(morgan('combined'))
 
@@ -11,6 +12,8 @@ router.use('/auth', authRoute)
 router.use('/users', userRoute)
 router.use('/activation', activationRoute)
 router.use('/todo-list', todoRoute)
+router.use('/forgot-password', ForgotPassword)
+router.use('/reset-password', ResetPassword)
 
 
 module.exports = router
